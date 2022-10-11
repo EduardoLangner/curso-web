5ca8a53e2208d90d485a530b10403ff82abeef8c
EduardoLangner <eduardodossantos752@gmail.com>
2022-10-11 15:00:09 -0300

    React module complete

--- react/react-project/src/index.js
+++ react/react-project/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import PrimeiroComponente from './componentes/primeiroComponente'
 import BomDia from './componentes/bomDia'
 import { BoaTarde, BoaNoite } from './componentes/multiplosComponentes'
+import Saudacao from './componentes/saudacao'
 
 
 ReactDOM.render( 
@@ -13,6 +14,7 @@ ReactDOM.render(
         <BomDia nome="João" />
         <BoaTarde nome="Maria" />
         <BoaNoite nome="José" />
+        <Saudacao tipo="Bom dia" nome="João" />
     </div>,
 
 document.getElementById('root')
